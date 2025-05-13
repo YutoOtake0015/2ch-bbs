40f10e6a231dda5d513ef98165997a9ba3fc91ce
YutoOtake <yuto.programming@gmail.com>
2025-05-13 23:03:31 +0900

    feat: Create base template

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <title>2ちゃんねる掲示板</title>
+  </head>
+  <body>
+    <header>
+      <h1 class="title">2ちゃんねる掲示板</h1>
+      <hr />
+    </header>
+    <div class="threadWrapper">
+      <div class="childWrapper">
+        <div class="threadTitle">
+          <span>【タイトル】</span>
+          <h1>作ってみる</h1>
+        </div>
+        <section>
+          <article>
+            <div class="wrapper">
+              <div class="nameArea">
+                <span>名前: </span>
+                <p class="username">わたし</p>
+                <time>: 2025/05/13 22:44 </time>
+              </div>
+              <p class="comment">手書きコメント</p>
+            </div>
+          </article>
+        </section>
+      </div>
+    </div>
+    <div class="threadWrapper">
+      <div class="childWrapper">
+        <div class="threadTitle">
+          <span>【タイトル】</span>
+          <h1>作ってみる</h1>
+        </div>
+        <section>
+          <article>
+            <div class="wrapper">
+              <div class="nameArea">
+                <span>名前: </span>
+                <p class="username">わたし</p>
+                <time>: 2025/05/13 22:44 </time>
+              </div>
+              <p class="comment">手書きコメント</p>
+            </div>
+          </article>
+        </section>
+        <form class="formWrapper">
+          <div>
+            <input type="submit" value="書き込む" />
+            <label>名前:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <textarea class="commentsTextArea"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
