5d4d5c0d8cb759b1d4de882474d097c585d97f04
YutoOtake <yuto.programming@gmail.com>
2025-05-15 23:44:32 +0900

    feat: database function

---
+++ app/database/connect.php
@@ -0,0 +1,13 @@
+<?php
+$user = "root";
+$pass = "";
+
+// DBと接続
+try {
+    // PDOオブジェクトを作成
+    $pdo = new PDO('mysql:host=localhost;dbname=2ch-bbs', $user, $pass);
+    echo "DBとの接続に成功しました";
+} catch (PDOException $e) {
+    // 接続失敗時のエラーメッセージ
+    echo $e->getMessage();  
+}
